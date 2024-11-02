e41b580a442963f19036e1c42617e969370d1f05
RyzhkovAA <qwerty@bebr.ru>
2024-11-02 10:09:04 +0500

    Добавьте файлы проекта.

---
+++ Cats.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cat
+{
+    internal class Cats
+    {
+        string name;
+        public string Name 
+        {
+            // получение значения
+            get { return name; }
+            // установка значения
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                //условие проверяет на то что в перемнной буквы и она не пустая
+                if (OnlyLetters&&value!="")
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+               
+            } 
+        }
+        
+        string ves;
+        public string Ves
+        {
+            get 
+            {
+                return ves;
+            }
+            set
+            {
+                //проверка на некоректность ввода и число не бесконечнность и 0<x!=0
+                if (Convert.ToDouble(value)>0)
+                {
+                    ves = value;
+                }
+                else
+                {
+                    Console.WriteLine("Weight is not correct");
+                }
+            }
+        }
+        
+
+        public void CName(string CatName)
+        {
+            Name = CatName;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+        }
+
+        public void CatInfo()
+        {
+            Console.WriteLine($"{name}-{ves}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cats Cat1 = new Cats();
+                Cats Cat2 = new Cats();
+               
+                Console.WriteLine("Write first cat name");
+                Cat1.Name=Console.ReadLine();
+                Console.WriteLine("Write second cat name");
+                Cat2.Name= Console.ReadLine();
+                Console.WriteLine("Write the weight of the first cat");
+                Cat1.Ves = Console.ReadLine();
+                Console.WriteLine("Write the weight of the second cat");
+                Cat2.Ves = Console.ReadLine();
+                
+
+                Cat1.Meow();
+                Cat2.Meow();
+                
+                Cat1.CatInfo();
+                Cat2.CatInfo();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            Console.Read();
+        }
+
+    }
+}
